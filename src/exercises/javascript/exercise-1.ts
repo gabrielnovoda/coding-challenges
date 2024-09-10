62c6dca9b8f274bc960b003755b1a92fa2fedf4f
Arun <arun.yogeshwaran@zenjob.com>
2024-09-10 10:41:55 +0200

    exercise1 tests passes

--- src/exercises/javascript/exercise-1.ts
+++ src/exercises/javascript/exercise-1.ts
@@ -2,7 +2,27 @@
  * Implement a function that takes a string of space separated numbers,
  * and returns a string with the highest and lowest number separated by space.
  *
- * Input: "1 2 -3 4 16"
+ * Input: "12-3416"
  * Output: "16 -3"
  */
-export const exercise1 = (input: string): string => {};
+// 
+export const exercise1 = (input: string): string => {
+    // let min = Number.MAX_VALUE;
+    // let max = Number.MIN_VALUE; // why this didn't work
+
+    let min = Infinity;
+    let max = -Infinity;
+
+    const numberList = input.trim().split(" ").map(Number);
+    console.log("NUMBERLIST =>>>>>>>", numberList);
+    for (const num of numberList) {
+        if (num > max) {
+            max = num;
+        }
+
+        if (num < min) {
+            min = num;
+        }
+    }
+    return `${max} ${min}`;
+};
